f8b519b91f7e6e3277cd9cfd6956a31bf39f1243
Edward Mitchell <efmitchell@gmail.com>
2016-04-20 11:45:15 +0100

    Implemented specialFunc() to double integer

--- newGitTest/mainfile.cpp
+++ newGitTest/mainfile.cpp
@@ -1,8 +1,15 @@
 #include "mainfile.h"
 #include <iostream>
 
+int specialFunc(int & inp) {
+	return (2*inp);
+}
+
 int main() {
 	std::cout << "Hi" << std::endl;
+
+	int a=3;
+	std::cout << "Input of 3 gives: " << specialFunc(a) << std::endl;
 	
 	std::system("pause");
 
