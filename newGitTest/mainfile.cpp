ecdc0523469bb181cfe8673a4e30c43e6c728507
Edward Mitchell <efmitchell@gmail.com>
2016-04-20 14:07:25 +0100

    added masterfunc()

--- newGitTest/mainfile.cpp
+++ newGitTest/mainfile.cpp
@@ -5,12 +5,19 @@ int specialFunc(int & inp) {
 	return (2*inp);
 }
 
+int masterfunc(void) {
+	std::cout <<"This is the masterfunc" << std::endl;
+	return 0;
+}
+
 int main() {
 	std::cout << "Hi" << std::endl;
 
 	int a=3;
 	std::cout << "Input of 3 gives: " << specialFunc(a) << std::endl;
 	
+	masterfunc();
+
 	std::system("pause");
 
 	return 0;
